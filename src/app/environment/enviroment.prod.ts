055ff34ad16d8ade1dcb9dd96a917bc1bf740b6b
vinic <viniciusrbastos@hotmail.com>
2025-06-26 17:57:55 -0300

    Feat: inserção de variavel de ambiente para prod.

---
+++ src/app/environment/enviroment.prod.ts
@@ -0,0 +1,10 @@
+const URL_BASE = 'https://esfsg-backend-cfdpfqgshmh5fydg.brazilsouth-01.azurewebsites.net/';
+const VERSAO = 'v1';
+const COMPLEMENTO = 'api';
+const URL_API = `${URL_BASE}/${COMPLEMENTO}`;
+
+export const ENVIRONMENT = {
+  URL_BASE,
+  VERSAO,
+  URL_API
+};
