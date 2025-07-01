d9419aeca186aa513d12de2d936e48085ec62a69
Matheus Victor <matheusvictorgdr15@gmail.com>
2025-07-01 17:18:53 -0300

    feat: teste leitor qrcode

--- src/app/presentation/pages/home-logada/home-logada.component.ts
+++ src/app/presentation/pages/home-logada/home-logada.component.ts
@@ -3,6 +3,9 @@ import { UsuarioAdminResponseDto } from '../../../core/application/dto/response/
 import { ParametroStorageEnum } from '../../../core/domain/enums/parametro-storage.enum';
 import { Router } from '@angular/router';
 import { Rotas } from '../../../core/domain/enums/rotas.enum';
+import { ZXingScannerComponent } from '@zxing/ngx-scanner';
+import { BrowserMultiFormatReader } from '@zxing/browser';
+import { BarcodeFormat, Result } from '@zxing/library';
 
 @Component({
   selector: 'app-home-logada',
@@ -18,9 +21,15 @@ export class HomeLogadaComponent {
 
   
   isSidebarOpen = false;
+  formatsEnabled: BarcodeFormat[] = [BarcodeFormat.QR_CODE];
 
   ngOnInit(){
     this.usuarioLogado = JSON.parse(localStorage.getItem(ParametroStorageEnum.USUARIO_LOGADO))
+    
+    BrowserMultiFormatReader.listVideoInputDevices().then(devices => {
+      this.availableDevices = devices;
+      this.selectedDevice = devices.find(d => /back|traseira/i.test(d.label)) || devices[0];
+    });
     
     // if(this.usuarioLogado === null || this.usuarioLogado === undefined) {
     //   console.error('Usuário não encontrado no armazenamento local.');
@@ -35,5 +44,18 @@ export class HomeLogadaComponent {
   navegar() {
     this.router.navigate([Rotas.HOME_LOGADA, Rotas.CHECK_IN]);
     this.toggleSidebar();
+  }
+
+  availableDevices: MediaDeviceInfo[] = [];
+  selectedDevice: MediaDeviceInfo | undefined;
+
+  handleQrCodeResult(result: string): void {
+    console.log('QR Code:', result);
+    // Aqui você pode tratar o valor lido
+  }
+
+  onDeviceSelect(event: Event): void {
+    const deviceId = (event.target as HTMLSelectElement).value;
+    this.selectedDevice = this.availableDevices.find(d => d.deviceId === deviceId);
   }
 }
